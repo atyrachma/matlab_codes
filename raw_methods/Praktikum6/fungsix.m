9c227d17327bc379e2aae4dee329351e729b71f1
Aty Rachmawati <atyrachmawati@gmail.com>
2016-04-11 14:48:28 +0700

    Create fungsix.m

---
+++ raw_methods/Praktikum6/fungsix.m
@@ -0,0 +1,37 @@
+clear all;
+clc;
+f=inline('x^2')
+a=-1
+b=2
+eps=10^-6
+fa=f(a)
+fb=f(b)
+maxiter=30;
+R(1,1)=(b-a)/2*(fa+fb);
+j=1;
+lanjut=true;
+while (j<=maxiter) && lanjut
+    n=2^j;
+    h=(b-a)/n;
+    s=0;
+    x=a;
+    for i=1:(n/2)
+        x=a+(2*i-1)*h;
+        s=s+f(x);
+    end
+    R(j+1,1)=(R(j,1))/2 + h*s
+    for k=1:j
+        R(j+1,k+1)=(4^k*R(j+1,k)-R(j,k))/(4^k-1)
+    end
+    galat=abs((R(j+1,j+1)-R(j,j))/R(j+1,j+1));
+    if galat <= eps
+        Rmbrg=R(j+1,j+1)
+        lanjut=false;
+    end
+    j=j+1;
+end
+
+if galat > eps
+    disp ('Proses Belum Konvergen')
+    Rmbrg=R(j-1,j-1)
+end
